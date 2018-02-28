92e5d65ebb387171103cecd9853525ca754f3c6f
Danilo Neves Cruz <cruzdanilo@gmail.com>
2018-02-28 14:23:05 -0300

    use arc rotate camera

--- src/HoroscopeScene.js
+++ src/HoroscopeScene.js
@@ -1,17 +1,16 @@
-import { Scene, FreeCamera, Vector3, HemisphericLight, MeshBuilder } from 'babylonjs';
+import { Scene, ArcRotateCamera, Vector3, HemisphericLight, MeshBuilder } from 'babylonjs';
 import nominatim from 'nominatim-browser';
 
 export default class HoroscopeScene extends Scene {
   constructor(engine) {
     super(engine);
 
-    this.camera = new FreeCamera('camera', new Vector3(0, 5, -10), this);
-    this.camera.setTarget(Vector3.Zero());
+    this.camera = new ArcRotateCamera('camera', 0, 1.2, 5, Vector3.Zero(), this);
     this.camera.attachControl(engine.getRenderingCanvas());
 
-    this.light = new HemisphericLight('light1', new Vector3(0, 1, 0), this);
+    this.light = new HemisphericLight('light0', new Vector3(0, 1, 0), this);
 
-    this.sphere = MeshBuilder.CreateSphere('sphere', { segments: 12, diameter: 1 }, this);
+    this.earth = MeshBuilder.CreateSphere('earth', { segments: 12, diameter: 1 }, this);
 
     nominatim.geocode({ city: 'sao paulo' })
       .then(r => console.log(r))
