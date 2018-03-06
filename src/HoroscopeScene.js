930b925609b60b820427e73f09c7a00a82e3e714
Danilo Neves Cruz <cruzdanilo@gmail.com>
2018-03-06 02:31:43 -0300

    add meridians and sun

--- src/HoroscopeScene.js
+++ src/HoroscopeScene.js
@@ -2,6 +2,7 @@ import {
   Animation,
   ArcRotateCamera,
   Axis,
+  Color3,
   HemisphericLight,
   Mesh,
   MeshBuilder,
@@ -20,45 +21,16 @@ export default class HoroscopeScene extends Scene {
     this.camera.upperRadiusLimit = 100;
     this.camera.attachControl(engine.getRenderingCanvas());
     this.light = new HemisphericLight('light0', new Vector3(1, 0, 0), this);
-    const sun = new Animation('sun', 'direction', 30, Animation.ANIMATIONTYPE_VECTOR3);
-    sun.setKeys([
-      {
-        frame: 0,
-        value: new Vector3(1, 0, 0),
-        outTangent: new Vector3(0, 0, 1),
-      },
-      {
-        frame: 15,
-        value: new Vector3(0, 0, 1),
-        outTangent: new Vector3(-1, 0, 0),
-      },
-      {
-        frame: 30,
-        value: new Vector3(-1, 0, 0),
-        outTangent: new Vector3(0, 0, -1),
-      },
-      {
-        frame: 45,
-        value: new Vector3(0, 0, -1),
-        outTangent: new Vector3(1, 0, 0),
-      },
-      {
-        frame: 60,
-        value: new Vector3(1, 0, 0),
-      },
-    ]);
-    this.beginDirectAnimation(this.light, [sun], 0, 120, true);
 
     this.earth = MeshBuilder.CreateSphere('earth', {}, this);
     const radius = 3;
-    const wireframe = new StandardMaterial('wireframe', this);
-    wireframe.wireframe = true;
-    this.equator = MeshBuilder.CreateCylinder('equator', {
-      diameter: radius * 2,
-      height: 0.001,
+    const width = 0.02;
+    this.equator = MeshBuilder.CreateTube('equator', {
+      radius,
+      path: [new Vector3(0, -width / 2, 0), new Vector3(0, width / 2, 0)],
+      sideOrientation: Mesh.DOUBLESIDE,
     }, this);
     this.equator.parent = this.earth;
-    this.equator.material = wireframe;
     this.axis = MeshBuilder.CreateLines('axis', {
       points: [new Vector3(0, -radius, 0), new Vector3(0, radius, 0)],
     }, this);
@@ -69,5 +41,31 @@ export default class HoroscopeScene extends Scene {
       sideOrientation: Mesh.DOUBLESIDE,
     }, this);
     this.zodiac.rotate(Axis.X, 23.5 * (Math.PI / 180), Space.LOCAL);
+    for (let i = 0; i < 6; i += 1) {
+      const meridian = MeshBuilder.CreateTube(`meridian${0}`, {
+        radius,
+        path: [new Vector3(0, 0, -width / 2), new Vector3(0, 0, width / 2)],
+        sideOrientation: Mesh.DOUBLESIDE,
+      }, this);
+      meridian.parent = this.earth;
+      meridian.rotate(Axis.Y, i * (Math.PI / 6), Space.LOCAL);
+    }
+    this.sun = MeshBuilder.CreateSphere('sun', { diameter: 0.2 }, this);
+    this.sun.parent = this.zodiac;
+    const material = new StandardMaterial('sun', this);
+    material.diffuseColor = Color3.Yellow();
+    this.sun.material = material;
+    const animation = new Animation('sun', 'position', 30, Animation.ANIMATIONTYPE_VECTOR3);
+    const keys = [];
+    for (let i = 0; i < 12; i += 1) {
+      const a = i * (Math.PI / 6);
+      keys.push({
+        frame: i * 10,
+        value: new Vector3(radius * Math.cos(a), 0, radius * Math.sin(a)),
+      });
+    }
+    keys.push({ frame: 120, value: keys[0].value });
+    animation.setKeys(keys);
+    this.beginDirectAnimation(this.sun, [animation], 0, 120, true);
   }
 }
